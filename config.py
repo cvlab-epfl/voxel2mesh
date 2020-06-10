03cc280a4a3c18f7d9b8bffa7ef8b1f7368939d4
Pamuditha Udaranga Wickramasinghe <udaranga.wickramasinghe@epfl.ch>
2020-06-10 08:32:27 +0200

    comments addeed

--- config.py
+++ config.py
@@ -15,17 +15,20 @@ def load_config(exp_id):
  
 
     ''' Save at '''
-    cfg.save_path = '/cvlabdata2/cvlab/datasets_udaranga/experiments/miccai2020/'
-    # cfg.save_path = '/home/wickrama/experiments/shrinknet'
+    cfg.save_path = '/your/path/to/experiments/miccai2020/' # results will be saved here
 
-    cfg.save_dir_prefix = 'Experiment_'
+    cfg.save_dir_prefix = 'Experiment_' # prefix for experiment folder
  
     cfg.name = 'voxel2mesh'
    
 
     ''' Dataset ''' 
     cfg.training_set_size = 10  
-    cfg.patch_shape = (64, 64, 64)
+
+    # input should be cubic. Otherwise, input should be padded accordingly.
+    cfg.patch_shape = (64, 64, 64) 
+    
+
     cfg.ndims = 3
     cfg.augmentation_shift_range = 10
 
@@ -33,7 +36,11 @@ def load_config(exp_id):
     cfg.first_layer_channels = 16
     cfg.num_input_channels = 1
     cfg.steps = 4
-    cfg.batch_size = 1
+
+    # Only supports batch size 1 at the moment. 
+    cfg.batch_size = 1 
+
+
     cfg.num_classes = 2
     cfg.batch_norm = True  
     cfg.graph_conv_layer_count = 4
@@ -44,6 +51,6 @@ def load_config(exp_id):
 
     ''' Training '''
     cfg.numb_of_itrs = 300000
-    cfg.eval_every = 1000
+    cfg.eval_every = 1000 # saves results to disk
     
     return cfg
